4e3b9f05e6a29e29907b83913db270f995749be8
Aleksandr Daniel <tgaleksandr@gmail.com>
2022-03-29 00:34:27 +0300

    Импортировал переменную для работы модуля

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { renderPhotos } from './mock/pictures.js';
 import { renderBigPicture } from './mock/big-picture.js';
 import { mockData } from './mock/data.js';
-
+import { uploadBtn } from './mock/form.js';
 
 renderPhotos(mockData);
 
