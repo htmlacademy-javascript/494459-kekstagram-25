46df77e0dbba300d6cdde5969eb9af13a38bc6cc
Keks <keks@htmlacademy.ru>
2022-04-04 19:40:30 +0300

    :heavy_check_mark: Сборка #9

--- 9/js/mock/form.js
+++ 9/js/mock/form.js
@@ -3,7 +3,8 @@ import { isEscapeKey, modalOpen, modalClose } from './utill.js';
 const uploadBtn = document.querySelector('.img-upload__input');
 const uploadOverlay = document.querySelector('.img-upload__overlay');
 const imgUploadCancelBtn = document.querySelector('.img-upload__cancel');
-const textHashtag = document.querySelector('.text__hashtags');
+const textHashtagInput = document.querySelector('.text__hashtags');
+const textDescriptionInput = document.querySelector('.text__description');
 const imgUploadForm = document.querySelector('.img-upload__form');
 // const submitForm = document.querySelector('.img-upload__submit');
 
@@ -32,33 +33,46 @@ function showUploadedPhoto() {
   uploadBtn.removeEventListener('change', showUploadedPhoto);
 }
 
-// const regExp = /^#[A-Za-zА-Я-а-яЁё0-9]{1,19}$/;
+const regExp = /^#[A-Za-zА-Я-а-яЁё0-9]{1,19}$/;
 
-// const onInputChange = (evt) => {
-//   const tmpResult = evt.target.value.split(' ');
-//   const isNoHaveRepeats = tmpResult.every((str, index, arr) => arr.indexOf(str) === index);
-//   const isHashtagLessThanFive = tmpResult.length <= 5;
-// };
+const onInputChange = (evt) => {
+  const stringArray = evt.target.value.split(' ');
+  const isNoHaveRepeats = stringArray.every((str, index, arr) => arr.indexOf(str) === index);
+  const isHashtagLessThanFive = stringArray.length <= 5;
+  return (isNoHaveRepeats && isHashtagLessThanFive && regExp.test(evt.target.value)) ? evt.target.value : false;
+};
 
 const pristine = new Pristine(imgUploadForm, {
-  classTo: 'form__text',
-  errorClass: 'form__text--invalid',
-  successClass: 'form__text--valid',
-  errorTextParent: 'form__text',
-  errorTextTag: 'span',
-  errorTextClass: 'form__error'
+  classTo: 'error-message__text',
+  errorClass: 'error-message__text--invalid',
+  successClass: 'error-message__text--valid',
+  errorTextParent: 'error-message__text',
+  errorTextTag: 'div',
+  errorTextClass: 'text__error'
 });
 
-const validateHashTags = (value) => value.length < 10;
+const validateHashTags = (value) => { // WIP
+  if (onInputChange) {
+    return value;
+  }
+};
 
-pristine.addValidator(textHashtag, validateHashTags, 'АстанАвись!!');
+const validateTextCount = (value) => value.length <= 140;
+
+pristine.addValidator(textHashtagInput, validateHashTags, 'АстАнАвись!! Ты что-то делаете не так!!');
+pristine.addValidator(textDescriptionInput, validateTextCount, 'Количество символов не может быть больше 140');
 
 imgUploadForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   pristine.validate();
 });
 
+const stopPropagation = (evt) => evt.stopPropagation();
+
+textHashtagInput.addEventListener('keydown', stopPropagation);
+textDescriptionInput.addEventListener('keydown', stopPropagation);
+
 uploadBtn.addEventListener('change', showUploadedPhoto);
-// textHashtag.addEventListener('change', onInputChange);
+textHashtagInput.addEventListener('change', onInputChange);
 
 export { uploadBtn };
